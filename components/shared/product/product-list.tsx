071bab1e501d3107424586bdce93da88395853c5
SrTrace <levasho04sergey@gmail.com>
2025-01-04 20:23:57 +0200

    added ProductCard

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,5 @@
+import ProductCard from "./product-card";
+
 const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?: number }) => {
   const limitedData = limit ? data.slice(0, limit) : data;
 
@@ -7,9 +9,7 @@ const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?
       {data.length > 0 ? (
         <div className="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {limitedData.map((product: any) => (
-            <div key={product.name}>
-              {product.name}
-            </div>
+            <ProductCard key={product.slug} product={product} />
           ))}
         </div>
       ) : (
