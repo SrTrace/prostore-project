8163e8612a31d90bf0a651bec282ec26fdd6466e
SrTrace <levasho04sergey@gmail.com>
2025-01-24 21:41:50 +0200

    customize jwt token callback

--- auth.ts
+++ auth.ts
@@ -54,13 +54,36 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // Set the user id from token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
+      console.log(token);
       // If there is an update, set the user name
       if (trigger === 'update') {
         session.user.name = user.name;
       }
       return session;
-    }
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+
+        // if user have no name then use a part of email
+        if (user.name === 'NO_NAME') {
+          token.name = user.email?.split('@')[0];
+
+          // update db reflet to the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          })
+        }
+      }
+
+      return token;
+    },
   },
 } satisfies NextAuthConfig;
 
