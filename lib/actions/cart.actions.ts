ee9336fa79f205e513d5d7b5c34b4174fd668437
SrTrace <levasho04sergey@gmail.com>
2025-01-27 14:33:08 +0200

    fix esling error in cart.actions

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -3,6 +3,7 @@
 import { CartItem } from "@/types";
 
 export async function addItemToCart(data: CartItem) {
+  const d = data;// temp line for eslint
   return {
     success: true,
     message: "Item added to cart",
