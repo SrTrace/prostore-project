d403cf24ca24d29f29fd9992f9b30adbad5795a8
SrTrace <levasho04sergey@gmail.com>
2025-01-29 15:54:46 +0200

    fix eslint error in auth.ts

--- auth.ts
+++ auth.ts
@@ -35,7 +35,10 @@ export const config = {
 
         // Check if user exitst and if the password matches
         if (user && user.password) {
-          const isMatch = await compare(credentials.password as string, user.password);
+          const isMatch = await compare(
+            credentials.password as string,
+            user.password
+          );
 
           // If password is correct return user
           if (isMatch) {
@@ -71,16 +74,45 @@ export const config = {
         token.id = user.id;
         token.role = user.role;
 
-        // if user have no name then use a part of email
+        // If user has no name then use the email
         if (user.name === 'NO_NAME') {
-          token.name = user.email?.split('@')[0];
+          token.name = user.email!.split('@')[0];
 
-          // update db reflet to the token name
+          // Update database to reflect the token name
           await prisma.user.update({
             where: { id: user.id },
             data: { name: token.name },
-          })
+          });
         }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
+      }
+
+      // Handle session updates
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name;
       }
 
       return token;
