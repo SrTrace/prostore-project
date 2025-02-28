e0ce2a916e74174b79a5b4136f5d713475d49f09
SrTrace <levasho04sergey@gmail.com>
2025-02-28 16:56:59 +0200

    update order buttons COD

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -22,15 +22,21 @@ import {
 import {
   createPayPalOrder,
   approvePayPalOrder,
+  updateOrderToPaidCOD,
+  deliverOrder,
 } from "@/lib/actions/order.actions";
 import { useToast } from "@/hooks/use-toast";
+import { useTransition } from "react";
+import { Button } from "@/components/ui/button";
 
 const OrderDetailsTable = ({
   order,
   paypalClientId,
+  isAdmin,
 }: {
   order: Order;
   paypalClientId: string;
+  isAdmin: boolean;
 }) => {
   const {
     shippingAddress,
@@ -82,6 +88,50 @@ const OrderDetailsTable = ({
       description: res.message,
     });
   };
+
+  // Button to mark order as paid
+  const MarkAsPaidButton = () => {
+    const [isPending, startTransition] = useTransition();
+    const { toast } = useToast();
+
+    return (
+      <Button
+        type='button'
+        disabled={isPending}
+        onClick={() => startTransition(async () => {
+          const res = await updateOrderToPaidCOD(order.id);
+          toast({
+            variant: res.success ? 'default' : 'destructive',
+            description: res.message,
+          });
+        })}
+      >
+        {isPending ? 'processing...' : 'Mark As Paid'}
+      </Button>
+    );
+  }
+
+  // Button to mark order as delivered 
+  const MarkAsDeliveredButton = () => {
+    const [isPending, startTransition] = useTransition();
+    const { toast } = useToast();
+
+    return (
+      <Button
+        type='button'
+        disabled={isPending}
+        onClick={() => startTransition(async () => {
+          const res = await deliverOrder(order.id);
+          toast({
+            variant: res.success ? 'default' : 'destructive',
+            description: res.message,
+          });
+        })}
+      >
+        {isPending ? 'processing...' : 'Mark As Delivered'}
+      </Button>
+    );
+  }
 
   return (
     <>
@@ -188,6 +238,19 @@ const OrderDetailsTable = ({
                   </PayPalScriptProvider>
                 </div>
               )}
+
+              {/* Cash on Delivery */}
+              {
+                isAdmin && !isPaid && paymentMethod === 'CashOnDelivery' && (
+                  <MarkAsPaidButton />
+                )
+              }
+              {
+                isAdmin && isPaid && !isDelivered && (
+                  <MarkAsDeliveredButton />
+                )
+              }
+
             </CardContent>
           </Card>
         </div>
