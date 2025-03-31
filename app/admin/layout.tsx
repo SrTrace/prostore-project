daa5e163bc6071bfce05832907ec4fd54baa1278
SrTrace <levasho04sergey@gmail.com>
2025-03-31 21:48:46 +0300

    add admin search form

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,6 +4,7 @@ import Menu from "@/components/shared/header/menu";
 import Link from "next/link";
 import MainNav from "./main-nav";
 import { Input } from "@/components/ui/input";
+import AdminSearch from "@/components/admin/admin-search";
 
 export default function AdminLayout({
   children,
@@ -25,13 +26,7 @@ export default function AdminLayout({
             </Link>
             <MainNav className='mx-6' />
             <div className="ml-auto flex items-center space-x-4">
-              <div>
-                <Input
-                  type="search"
-                  placeholder="Search..."
-                  className="md:w-[100px] lg:w-[300px]"
-                />
-              </div>
+              <AdminSearch />
               <Menu />
             </div>
           </div>
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -34,20 +34,74 @@ export async function getProductById(productId: string) {
 
 // Get all products
 export async function getAllProducts({
-  // query,
+  query,
   limit = PAGE_SIZE,
   page,
-  // category
+  category,
+  price,
+  rating,
+  sort,
 }: {
   query: string;
   limit?: number;
   page: number;
   category?: string;
+  price?: string;
+  rating?: string;
+  sort?: string;
 }) {
+  // Query filter
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== 'all'
+      ? {
+        name: {
+          contains: query,
+          mode: 'insensitive',
+        } as Prisma.StringFilter,
+      }
+      : {};
+
+  // Category filter
+  const categoryFilter = category && category !== 'all' ? { category } : {};
+
+  // Price filter
+  const priceFilter: Prisma.ProductWhereInput =
+    price && price !== 'all'
+      ? {
+        price: {
+          gte: Number(price.split('-')[0]),
+          lte: Number(price.split('-')[1]),
+        },
+      }
+      : {};
+
+  // Rating filter
+  const ratingFilter =
+    rating && rating !== 'all'
+      ? {
+        rating: {
+          gte: Number(rating),
+        },
+      }
+      : {};
+
   const data = await prisma.product.findMany({
-    orderBy: { createdAt: 'desc' },
+    where: {
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    },
+    orderBy:
+      sort === 'lowest'
+        ? { price: 'asc' }
+        : sort === 'highest'
+          ? { price: 'desc' }
+          : sort === 'rating'
+            ? { rating: 'desc' }
+            : { createdAt: 'desc' },
     skip: (page - 1) * limit,
-    take: limit
+    take: limit,
   });
 
   const dataCount = await prisma.product.count();
@@ -55,8 +109,32 @@ export async function getAllProducts({
   return {
     data,
     totalPages: Math.ceil(dataCount / limit),
-  }
+  };
 }
+// export async function getAllProducts({
+//   query,
+//   limit = PAGE_SIZE,
+//   page,
+//   category
+// }: {
+//   query: string;
+//   limit?: number;
+//   page: number;
+//   category?: string;
+// }) {
+//   const data = await prisma.product.findMany({
+//     orderBy: { createdAt: 'desc' },
+//     skip: (page - 1) * limit,
+//     take: limit
+//   });
+//
+//   const dataCount = await prisma.product.count();
+//
+//   return {
+//     data,
+//     totalPages: Math.ceil(dataCount / limit),
+//   }
+// }
 
 // Delete a product
 export async function deleteProduct(id: string) {
