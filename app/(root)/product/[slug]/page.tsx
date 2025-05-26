4a7fad7fa5348021ede4c0f5ea1351a97256ee74
SrTrace <levasho04sergey@gmail.com>
2025-05-26 22:13:08 +0300

    display reviews

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -8,6 +8,7 @@ import { notFound } from "next/navigation";
 import { getMyCart } from "@/lib/actions/cart.actions";
 import ReviewList from "./review-list";
 import { auth } from "@/auth";
+import Rating from "@/components/shared/product/rating";
 
 const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) => {
   const { slug } = await props.params;
@@ -36,9 +37,8 @@ const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) 
                 {product.brand} {product.category}
               </p>
               <h1 className="h3-bold">{product.name}</h1>
-              <p>
-                {product.rating} of {product.numReviews} Reviews
-              </p>
+              <Rating value={Number(product.rating)} />
+              <p>{product.numReviews} reviews</p>
               <div className="flex flex-col sm:flex-row sm:items-center gap-3">
                 <ProductPrice value={Number(product.price)}
                   className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -1,8 +1,15 @@
 'use client';
-import { Review } from "@prisma/client";
+
+import { useEffect } from "react";
+import { Review } from "@/types";
 import Link from "next/link";
 import { useState } from "react";
 import ReviewForm from "./review-form";
+import { getReviews } from "@/lib/actions/review.actions";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Calendar, User } from "lucide-react";
+import { formatDateTime } from "@/lib/utils";
+import Rating from "@/components/shared/product/rating";
 
 const ReviewList = ({ userId, productId, productSlug }: {
   userId: string;
@@ -11,8 +18,18 @@ const ReviewList = ({ userId, productId, productSlug }: {
 }) => {
   const [reviews, setReviews] = useState<Review[]>([]);
 
-  const reload = () => {
-    console.log('Review submitted');
+  useEffect(() => {
+    const loadReviews = async () => {
+      const res = await getReviews({ productId });
+      setReviews(res.data);
+    };
+
+    loadReviews();
+  }, [productId]);
+
+  const reload = async () => {
+    const res = await getReviews({ productId });
+    setReviews([...res.data]);
   };
 
   return (
@@ -33,7 +50,29 @@ const ReviewList = ({ userId, productId, productSlug }: {
         )
       }
       <div className="flex flex-col gap-3">
-        {/* Reviews here */}
+        {reviews.map((review) => (
+          <Card key={review.id}>
+            <CardHeader>
+              <div className="flex-between">
+                <CardTitle>{review.title}</CardTitle>
+              </div>
+              <CardDescription>{review.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex space-x-4 text-sm text-muted-foreground">
+                <Rating value={review.rating} />
+                <div className="flex items-center">
+                  <User className="mr-1 h-3 w-3" />
+                  {review.user ? review.user.name : "User"}
+                </div>
+                <div className="flex items-center">
+                  <Calendar className="mr-1 h-3 w-3" />
+                  {formatDateTime(review.createdAt).dateTime}
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </div>
   )
