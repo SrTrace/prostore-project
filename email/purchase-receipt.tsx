05a25055f9534bf0d4c791b5d2eec39412438776
SrTrace <levasho04sergey@gmail.com>
2025-07-05 11:50:20 +0300

    preview email in browser with React Email

--- email/purchase-receipt.tsx
+++ email/purchase-receipt.tsx
@@ -1,11 +1,59 @@
-import { Body, Column, Container, Head, Html, Img, Preview, Row, Section, Tailwind, Text } from "@react-email/components";
+import { Body, Column, Container, Head, Heading, Html, Img, Preview, Row, Section, Tailwind, Text } from "@react-email/components";
 import { Order } from "@/types";
-import { Heading } from "lucide-react";
 import { formatCurrency } from "@/lib/utils";
+import sampleData from "@/db/sample-data";
+require('dotenv').config();
+
+PurchaseReceiptEmail.PreviewProps = {
+  order: {
+    id: crypto.randomUUID(),
+    userId: '123',
+    user: {
+      name: 'John Doe',
+      email: 'test@test.com',
+    },
+    paymentMethod: 'Stripe',
+    shippingAddress: {
+      fullName: 'John Doe',
+      streetAddress: '123 Main st',
+      city: 'New York',
+      postalCode: '10001',
+      country: 'US',
+    },
+    createdAt: new Date(),
+    totalPrice: '100',
+    taxPrice: '10',
+    shippingPrice: '10',
+    itemsPrice: '80',
+    orderitems: sampleData.products.map((x) => ({
+      name: x.name,
+      orderId: '123',
+      productId: '123',
+      slug: x.slug,
+      qty: x.stock,
+      image: x.images[0],
+      price: x.price.toString(),
+    })),
+    isDelivered: true,
+    deliveredAt: new Date(),
+    isPaid: true,
+    paidAt: new Date(),
+    paymentResult: {
+      id: '123',
+      status: 'succeeded',
+      pricePaid: '100',
+      email_address: 'test@test.com',
+    },
+  },
+} satisfies OrderInformationProps;
 
 const dateFormatter = new Intl.DateTimeFormat('en', { dateStyle: 'medium' });
 
-const PurchaseReceiptEmail = ({ order }: { order: Order }) => {
+type OrderInformationProps = {
+  order: Order;
+};
+
+export default function PurchaseReceiptEmail({ order }: OrderInformationProps) {
   return (
     <Html>
       <Preview>View order receipt</Preview>
@@ -80,4 +128,3 @@ const PurchaseReceiptEmail = ({ order }: { order: Order }) => {
   );
 };
 
-export default PurchaseReceiptEmail; 
