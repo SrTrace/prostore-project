6dba5137bbb7f53f17fb221126a850ac1edada67
SrTrace <levasho04sergey@gmail.com>
2025-07-06 11:06:13 +0300

    add iconboxes component to main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
 import ViewAllProductsButton from "@/components/view-all-products-button";
 import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
+import IconBoxes from "@/components/icon-boxes";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -18,6 +19,7 @@ const Homepage = async () => {
         limit={4}
       />
       <ViewAllProductsButton />
+      <IconBoxes />
     </>
   )
 };
